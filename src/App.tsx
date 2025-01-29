7980fb903b9b0f8bed796b5f24be3090b90443c3
Kevin CS <kevinchandra031@gmail.com>
2025-01-29 15:35:07 +0700

    Install and setup PostCSS for Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      <h1 className="font-light text-4xl">Hello World!</h1>
+      <div className="absolute h-64 w-64 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-3 bg-slate-100 rounded-md shadow-md text-center">
+        <h1 className="font-light text-4xl">Hello World!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente neque architecto repellat debitis exercitationem cupiditate fuga voluptate vero quis consectetur?</p>
+      </div>
     </>
   )
 }
