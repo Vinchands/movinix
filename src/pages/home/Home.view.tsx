d46fc506389fbf3b58b1fc7b15220031fbac7432
Kevin CS <kevinchandra031@gmail.com>
2025-01-29 16:09:34 +0700

    Add home page for initialization

---
+++ src/pages/home/Home.view.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <div className="absolute min-w-72 h-72 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 bg-slate-100 rounded-md shadow-md text-center">
+        <h1 className="font-light text-4xl">Home Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dicta debitis deserunt, dolore praesentium a neque harum voluptatum? Nulla, repellendus.</p>
+      </div>
+    </>
+  )
+}
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
+import { Home } from '@/pages'
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" />
+        <Route index path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
